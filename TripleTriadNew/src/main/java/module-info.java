09448eaa99d5782e9210127f366b2c7363936e7c
Shane Reall <Shaneareall@gmail.com>
2024-04-26 11:04:20 -0400

    Final Version Update
    Should be Everything I wanted and needed in this Games, I also removed the extra Client that was used for Testing purposes

--- TripleTriadNew/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tripletriadnew {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tripletriadnew to javafx.fxml;
-    exports com.example.tripletriadnew;
-}
